eba3bc992d91a4599e263478b9a74627ae1e21f7
Wen Liang Goh <wlgoh.2020@scis.smu.edu.sg>
2025-01-04 11:18:13 +0800

    feature(img): Add kepubify

--- image/dedrm_all.sh
+++ image/dedrm_all.sh
@@ -5,8 +5,17 @@ if [ -z "$( ls -A '/home/libgourou/.adept' )" ]; then
    echo "y" | adept_activate --anonymous
 fi
 
+# Process ACSM to EPUB
 for file in /home/libgourou/input/*.acsm
 do
   echo "Processing $file"
   /home/libgourou/dedrm_one.sh "${file}"
+done
+
+# Process EPUB to KEPUB
+mkdir -p /home/libgourou/output_kepub
+for file in /home/libgourou/output/*.epub
+do
+  echo "Converting EPUB to KEPUB for $file"
+  ./kepubify-linux-64bit "${file}" -o /home/libgourou/output_kepub
 done
--- image/dedrm_one.sh
+++ image/dedrm_one.sh
@@ -7,4 +7,6 @@ acsmdownloader -f ${acsm_file} -O ${acsm_folder}
 file_name=$(ls ${acsm_folder})
 adept_remove -f "${acsm_folder}/${file_name}"
 
+./kepubify-linux-64bit "${acsm_folder}/${file_name}"
+
 cp "${acsm_folder}/${file_name}" /home/libgourou/output
